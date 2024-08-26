135c7f59387c035a3dd25679a823adfd15e9ade8
hunterCM <inayethg777@gmail.com>
2024-08-26 19:37:15 +0200

    Delete deploy-nextjs-azure-main directory

--- deploy-nextjs-azure-main/pages/api/signin.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.redirect(
-    "https://miro.com/oauth/authorize?response_type=code&client_id=" +
-      process.env.clientID +
-      "&redirect_uri=" +
-      process.env.redirectURL,
-  );
-}
